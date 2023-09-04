70e7ac021733fdd9f6504d189a797e689079fe36
ismaelTV <is.tapiav@duocuc.cl>
2023-09-04 19:54:52 -0300

    restablecer listo

--- src/app/pages/restablecer/restablecer.page.ts
+++ src/app/pages/restablecer/restablecer.page.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestablecerPage implements OnInit {
 
+  formRestablecer = {
+  usuario:""
+  }
+
   constructor() { }
 
   ngOnInit() {
